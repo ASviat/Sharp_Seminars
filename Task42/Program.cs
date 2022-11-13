3d36c406a46b66679259361558de8ce73829f7dd
Sviatoslav <ecrprague.asi@gmail.com>
2022-11-14 00:00:33 +0300

    Fking Task Done

---
+++ Task42/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное. 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+//BinaryDigit(num);
+Console.WriteLine($"{BinaryDigit(num)}  0  ");
+
+int BinaryDigit(int number)
+{
+    
+   int digit = number / 2;
+    
+    if (digit>0)
+    {
+        Console.Write($"{number%2}  ");
+        return BinaryDigit(digit);
+    }
+    else return 1;
+}
+
+//chnya
